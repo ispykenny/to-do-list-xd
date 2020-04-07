1ee6fbd4ad462bdd15637bf55ebd7ed96f701549
atokad5 <kenny.krosky@gmail.com>
2020-04-07 06:59:33 -0700

    working on checkboxhacK

--- components/checkboxChecker.js
+++ components/checkboxChecker.js
@@ -3,22 +3,23 @@ const $ = require('../lib/jquery');
 const storageHelper = require('../lib/storage-helper');
 
 module.exports.checkboxChecker = async (event) => {
-  let cleanItem = state.data;
-  let $this = $(event.currentTarget);
-  let checked = $this.parents('.to-do-item').hasClass('is-checked')
+  const cleanItem = state.data;
+  const $this = event.currentTarget;
+  const checked  = $this.parentNode.parentNode.classList.contains('is-checked');
+  
 
   if (checked) {
-    $this.find('.checker').attr('checked', false)
-    $this.parents('.to-do-item').removeClass('is-checked')
+    $this.querySelector('.checker').setAttribute('checked', false)
+    $this.parentNode.parentNode.classList.remove('is-checked');
   } else {
-    $this.find('.checker').attr('checked', true)
-    $this.parents('.to-do-item').addClass('is-checked')
+    $this.querySelector('.checker').setAttribute('checked', true)
+    $this.parentNode.parentNode.classList.add('is-checked')
   }
 
-  if (cleanItem[$this.parents('.to-do-item').index()].done) {
-    cleanItem[$this.parents('.to-do-item').index()].done = false
-  } else {
-    cleanItem[$this.parents('.to-do-item').index()].done = true
-  }
+  // if (cleanItem[$this.parents('.to-do-item').index()].done) {
+  //   cleanItem[$this.parents('.to-do-item').index()].done = false
+  // } else {
+  //   cleanItem[$this.parents('.to-do-item').index()].done = true
+  // }
   storageHelper.set('weee', cleanItem);
 }
